1bc5cc5776c9d58734e91e380ebe4a0ed7b639e7
Akshay Raju <166707593+deappy@users.noreply.github.com>
2024-07-06 05:03:29 +0000

    datatypes

---
+++ basics/02datatypes.js
@@ -0,0 +1,22 @@
+"use strict";// treat  all JS code as newer version
+
+// alert(3+3) error:since we are using node JS not the browser
+
+console.log(3+3)
+console.log("akshay\n")
+
+let country = "India"
+let age = 21
+// number ==> 2 power 53
+// brigint
+// string ""
+// boolean ==> true/false
+// null ==> standalone value, can assign value to var
+// undefined ==>
+// symbol ==> unique
+
+
+// object
+
+console.log(typeof undefined)
+console.log(typeof null)
