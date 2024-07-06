55a9df8979fa7798a8fdb26d7905d2a2d86af0c8
Akshay Raju <166707593+deappy@users.noreply.github.com>
2024-07-06 04:41:57 +0000

    01variables file

---
+++ basics/01variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "akshay@google.com"
+var accountPaswword = "12345"
+accountCity = "Jaipur"
+let accountState;
+// accountId = 2 (not possible)
+
+accountEmail = "appu@gmail.com"
+accountPaswword = "2121212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPaswword,accountCity,accountState])
