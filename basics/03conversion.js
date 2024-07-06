fdaf7b3e9c9b44daabc2b2d34e430f40194d3194
Akshay Raju <166707593+deappy@users.noreply.github.com>
2024-07-06 05:52:35 +0000

    conversion

---
+++ basics/03conversion.js
@@ -0,0 +1,18 @@
+let score =undefined
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" =? NaN
+// true => 1
+// null => 0
+// undefined => NaN
+console.log(" ")
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
